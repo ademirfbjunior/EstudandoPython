27211d5e0824378f46e76a4f468c887c26cff8ee
Ademir Junior <ademir.jun@hotmail.com>
2022-04-05 20:54:54 -0300

    criando um programa pra imprmir valor int por extenso

---
+++ testando uns negocios/numeroExtenso.py
@@ -0,0 +1,13 @@
+while True:
+    primeirosNumeros = ["um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","dose","treze","quatorze","quinze","dezeseis","dezesete","dezoito","dezenove"]
+
+    reposta = ""
+    valor=int(input("Diite um valor: "))
+    if valor == 0 :
+        reposta = "zero"
+    for i in range(valor):
+        resposta = primeirosNumeros[i]
+        
+    print(resposta)
+
+    
