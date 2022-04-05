ff5cddb1525d07459295597bf73299411787e6e5
Ademir Junior <ademir.jun@hotmail.com>
2022-04-05 19:32:33 -0300

    testando um neogicio

--- exemploObjeto/Metodos.py
+++ exemploObjeto/Metodos.py
@@ -39,3 +39,14 @@ class metodos:
                         break
             print(meuTexto)        
             return resposta 
+aaaAAaaaAAaaaAAaaaAAaaaAAaaaAA
+aaaAA
+aaaAA
+
+aaaAA
+aaaAA
+aaaAA
+aaaAA
+aaaAA
+aaaAA
+aaaAA
