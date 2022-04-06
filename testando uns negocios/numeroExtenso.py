46ceeccfc056a77ab561a4504a53b71d5b5942f1
Ademir Junior <ademir.jun@hotmail.com>
2022-04-06 09:08:42 -0300

    atualzando numerosExtenso

--- testando uns negocios/numeroExtenso.py
+++ testando uns negocios/numeroExtenso.py
@@ -1,13 +1,66 @@
 while True:
-    primeirosNumeros = ["um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","dose","treze","quatorze","quinze","dezeseis","dezesete","dezoito","dezenove"]
+    primeirosNumeros = ["zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","dose","treze","quatorze","quinze","dezeseis","dezesete","dezoito","dezenove"]
 
     reposta = ""
-    valor=int(input("Diite um valor: "))
-    if valor == 0 :
-        reposta = "zero"
-    for i in range(valor):
-        resposta = primeirosNumeros[i]
-        
-    print(resposta)
-
+    valor=int(input("Digite um valor: "))
     
+    if valor < 20:
+        resposta = primeirosNumeros[i]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+
+    elif valor < 30:
+        if valor == 20:
+            resposta = "vinte"
+        else:
+            resposta = "vinte e", primeirosNumeros[valor[1]]
+
+            
+    print(resposta)
+   
