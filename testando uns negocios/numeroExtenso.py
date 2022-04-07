5139ee190ee62fb9c465ca8e44be46c0de4e3cdd
Ademir Junior <ademir.jun@hotmail.com>
2022-04-07 14:59:16 -0300

    começando a adicionar centenas para os numeros por extenso

--- testando uns negocios/numeroExtenso.py
+++ testando uns negocios/numeroExtenso.py
@@ -1,59 +1,72 @@
 
-primeirosNumeros = ["zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","dose","treze","quatorze","quinze","dezeseis","dezesete","dezoito","dezenove"]
-
-resposta = ""
-# valor=int(input("Digite um valor: "))
-for valor in range(100):
+def unidade_dezena (valor):
+    #unidade
+    primeirosNumeros = ["zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","dose","treze","quatorze","quinze","dezeseis","dezesete","dezoito","dezenove"]
+    # resposta = ""
+    # valor=int(input("Digite um valor: "))
     unidade = valor // 1 % 10
     if valor < 20:
-        resposta = primeirosNumeros[valor]
+        return  primeirosNumeros[valor]
     #vinte
     elif valor < 30:
         if valor == 20:
-            resposta = "vinte"
+            return "vinte"
         else:
-            resposta = "vinte e " + primeirosNumeros[unidade]
+            return "vinte e " + primeirosNumeros[unidade]
     #Trinta            
     elif valor < 40:
         if valor == 30:
-            resposta = "trinta"
+            return "trinta"
         else:
-            resposta = "trinta e " + primeirosNumeros[unidade]
+            return "trinta e " + primeirosNumeros[unidade]
     #Quarenta
     elif valor < 50:
         if valor == 40:
-            resposta = "quarenta"
+            return "quarenta"
         else:
-            resposta = "quarenta e " + primeirosNumeros[unidade]
+            return "quarenta e " + primeirosNumeros[unidade]
     #Cinquenta    
     elif valor < 60:
         if valor == 50:
-            resposta = "cinquenta"
+            return "cinquenta"
         else:
-            resposta = "cinquenta e " + primeirosNumeros[unidade]
+            return "cinquenta e " + primeirosNumeros[unidade]
     #Sessenta
     elif valor < 70:
         if valor == 60:
-            resposta = "sessenta"
+            return "sessenta"
         else:
-            resposta = "sessenta e " + primeirosNumeros[unidade]
+            return "sessenta e " + primeirosNumeros[unidade]
     #Setenta
     elif valor < 80:
         if valor == 70:
-            resposta = "setenta"
+            return "setenta"
         else:
-            resposta = "setenta e " + primeirosNumeros[unidade]
+            return "setenta e " + primeirosNumeros[unidade]
     #oitenta
     elif valor < 90:
         if valor == 80:
-            resposta = "oitenta"
+            return "oitenta"
         else:
-            resposta = "oitenta e " + primeirosNumeros[unidade]
+            return "oitenta e " + primeirosNumeros[unidade]
     #noventa   
     elif valor < 100:
         if valor == 90:
-            resposta = "noventa"
+            return "noventa"
         else:
-            resposta = "noventa e " + primeirosNumeros[unidade]
+            return "noventa e " + primeirosNumeros[unidade]
 
-    print(resposta)
+# for i in range(100):
+#     print(unidade_dezena(i))
+
+def centena(valor):
+    
+    if valor == 100:
+        return "Cem"
+    else:
+        return "cento e " + unidade_dezena(valor)
+    
+
+while True:
+    valor = int(input("\nDigite um numero: "))
+    print(centena(valor))
