64a73828650b792afd05dcf0b4643ff0e2f77d92
Ademir Junior <ademir.jun@hotmail.com>
2022-04-13 19:58:20 -0300

    Apredendo Modulos built

---
+++ cursoPython/modulo_built1.py
@@ -0,0 +1,21 @@
+import math
+#Modulo Math.ceil. Redonda o valor floar para cima
+print ("Utilizando Math.ceil\n",math.ceil(4.1))
+#Result = 5
+
+#Math.floor redonda para baixo.
+print("Utilizando math.floor\n", math.floor(3.9))
+#Result = 3
+
+#math.fsum Soma todos valor de uma lista
+print("Utilizando math.fsum\n", math.fsum([1,2,3,4,5,6,7]))
+#Result = 28
+
+#math.sqrt() calcula a raiz quadrada
+print("Utilizando math.sqrt\n", math.sqrt(16))
+#result = 4
+
+import time
+
+#time.localtime() horario, data e contagem de tempo
+print("\nUtilizando time.localTime", time.localtime())
